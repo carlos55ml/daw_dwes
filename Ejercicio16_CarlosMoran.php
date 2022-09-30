391766f8a06eb26c4c0b836a1469057795b577d3
carlos55ml <cmoranlozano55@gmail.com>
2022-09-30 12:59:24 +0200

    16 17 18

--- Ejercicio16_CarlosMoran.php
+++ Ejercicio16_CarlosMoran.php
@@ -4,5 +4,19 @@ $nums = array();
 for ($i=0; $i < 7; $i++) { 
     $nums[$i] = rand(0,50);
 }
+print_r($nums);
+sort($nums);
+
+$menor = $nums[0];
+$mayor = $nums[count($nums)-1];
+
+$total = 0;
+foreach ($nums as $val) {
+    $total += $val;
+}
+
+$media = $total / count($nums);
+
+echo " <br> Num mayor: $mayor Num menor: $menor Total: $total Media: $media";
 
 ?>
