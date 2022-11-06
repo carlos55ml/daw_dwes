99e035f3ef622e21dc56ae53587da0a205939404
carlos55ml <cmoranlozano55@gmail.com>
2022-11-06 19:00:05 +0100

    moved and progress

--- tema_02/tema02_ampliacion_CarlosMoran/ejercicio01/toolsArray.php
+++ tema_02/tema02_ampliacion_CarlosMoran/ejercicio01/toolsArray.php
@@ -1,5 +1,23 @@
 <?php
 
+function imprArr2($array) {
+  foreach ($array as $key => $value) {
+      echo "<br> \"$key\" = { ";
+      foreach ($value as $key2 => $value2) {
+          echo "\"$key2\" = \"$value2\", ";
+      }
+      echo " }";
+  }
+}
+
+function imprArr($array) {
+  echo "<br> { ";
+  foreach ($array as $key => $value) {
+      echo "\"$key\" = \"$value\", ";
+  }
+  echo " }";
+}
+
 /**
  * fillArray(int cant)
  * @param {int} cant - tamaño del array deseado
@@ -23,5 +41,31 @@ function valuesExists($arr, $value) {
   return $pos;
 }
 
+function genArrayAndNewValue($cant, $newVal) {
+  $arr = fillArray($cant);
+  $arrExist = valuesExists($arr, $newVal);
+  if ($arrExist) {
+    echo "El valor $newVal ya existe en el array, en las posiciones: ";
+    echo "".implode(", ", $arrExist);
+  } else {
+    echo "El valor $newVal no existe, añadiendo...<br>";
+    array_push($arr, $newVal);
+  }
+  return $arr;
+}
+
+function genArrayAndNewValueToBegging($cant, $newVal) {
+  $arr = fillArray($cant);
+  $arrExist = valuesExists($arr, $newVal);
+  if ($arrExist) {
+    echo "El valor $newVal ya existe en el array, en las posiciones: ";
+    print_r($arrExist);
+    imprArr($arrExist);
+  } else {
+    echo "El valor $newVal no existe, añadiendo al principio del array...<br>";
+    array_unshift($arr, $newVal);
+  }
+  return $arr;
+}
 
 ?>
---
+++ tema_02/tema02_ampliacion_CarlosMoran/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Index</title>
+</head>
+<body>
+  <h1>Test</h1>
+  <?php
+  include("./ejercicio01/toolsArray.php");
+
+  echo "<br>Array de tamano 4: ";
+  $array4 = fillArray(4);
+  imprArr($array4);
+
+  echo "<br>Array con nuevo valor: ";
+  $arrayNew = genArrayAndNewValue(8, 5);
+  imprArr($arrayNew);
+
+  ?>
+</body>
+</html>
